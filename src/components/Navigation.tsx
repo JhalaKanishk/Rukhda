e5db4b5833d1709418d82681ee045f51fc9f558e
Kanishk Raj Singh Jhala <kanishkrajsinghjhala@gmail.com>
2025-07-27 10:34:49 +0530

    Fixed on Beta Serve error

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
-import { Leaf, Menu, X } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 
